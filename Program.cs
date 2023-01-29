9575ce2ff91270c4d93c842debab55a67a101123
MartinKulev <56129939+MartinKulev@users.noreply.github.com>
2023-01-30 01:06:05 +0200

    Wrong input protection + removed half of the if else statemnts

--- Program.cs
+++ Program.cs
@@ -1,216 +1,280 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace tictactoe
+namespace TicTacToe._2
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            string firstRow  = "   |   |  ";
-            string secondRow = "   |   |  ";
-            string thirdRow  = "   |   |  ";
-            Console.WriteLine(firstRow);
-            Console.WriteLine(secondRow);
-            Console.WriteLine(thirdRow);
-            string currentChar = string.Empty;
-            int x = int.Parse(Console.ReadLine());
-            for (int i = 0; i < 10; i++)
+        {           
+            char symbol = ' ';
+            int n = 0;
+            List<char> firstRow = "│   │   │   │".ToList();
+            List<char> secondRow = "│   │   │   │".ToList();
+            List<char> thirdRow = "│   │   │   │".ToList();
+            foreach (char c in firstRow)
             {
-                if(i % 2 == 0)
+                Console.Write(c);
+            }
+            Console.WriteLine();
+            foreach (char c in secondRow)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+            foreach (char c in thirdRow)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+            for (int a = 0; a < 9; a++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                bool dontClear = false;
+                try
                 {
-                    currentChar = "X";
+                    n = int.Parse(Console.ReadLine());
 
                 }
-                else if(i % 2 == 1)
+                catch { ArgumentException er; }
                 {
-                    currentChar = "0";
-                }
-                    if (x == 1)
-                    {
-                        firstRow = firstRow.Insert(2, currentChar);
-                        firstRow = firstRow.Remove(0, 1);
-                    }
-                    else if (x == 2)
-                    {
-                        firstRow = firstRow.Insert(6, currentChar);
-                        firstRow = firstRow.Remove(5, 1);
-                    }
-                    else if (x == 3)
-                    {
-                        firstRow = firstRow.Insert(9, currentChar);
-
-                    }
-                    else if (x == 4)
-                    {
-                        secondRow = secondRow.Insert(2, currentChar);
-                        secondRow = secondRow.Remove(0, 1);
-                    }
-                    else if (x == 5)
-                    {
-                        secondRow = secondRow.Insert(6, currentChar);
-                        secondRow = secondRow.Remove(5 , 1);
-                    }
-                    else if (x == 6)
-                    {
-                        
-                        secondRow = secondRow.Insert(9, currentChar);
-                        
-                    }
-                    else if (x == 7)
-                    {
-                        
-                        thirdRow = thirdRow.Insert(2, currentChar);
-                        thirdRow = thirdRow.Remove(0, 1);
-                    }
-                    else if (x == 8)
-                    {
-                        thirdRow = thirdRow.Insert(6, currentChar);
-                        thirdRow = thirdRow.Remove(5 , 1);
-                    }
-                    else if (x == 9)
-                    {
-                        thirdRow = thirdRow.Insert(9, currentChar);
 
                 }
-                Console.WriteLine(firstRow);
-                Console.WriteLine(secondRow);
-                Console.WriteLine(thirdRow);
-                if(firstRow[1] == 'X' & firstRow[5] == 'X' & firstRow[9] == 'X')
+                if (a % 2 == 0)
                 {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine(" X | X | X ");
-                    Console.WriteLine("   |   |   ");
-                    Console.WriteLine("   |   |   ");
+                    symbol = 'X';
+                }
+                else
+                {
+                    symbol = 'O';
+                }
+                if (n == 1)
+                {
+                    if(firstRow[2] == 'X' || firstRow[2] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        firstRow[2] = symbol;
+                    }                 
+                }
+                else if (n == 2)
+                {
+                    if (firstRow[6] == 'X' || firstRow[6] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        firstRow[6] = symbol;
+                    }
+                }
+                else if (n == 3)
+                {
+                    if (firstRow[10] == 'X' || firstRow[10] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        firstRow[10] = symbol;
+                    }
+                }
+                else if (n == 4)
+                {
+                    if (secondRow[2] == 'X' || secondRow[2] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        secondRow[2] = symbol;
+                    }
+                }
+                else if (n == 5)
+                {
+                    if (secondRow[6] == 'X' || secondRow[6] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        secondRow[6] = symbol;
+                    }
+                }
+                else if (n == 6)
+                {
+                    if (secondRow[10] == 'X' || secondRow[10] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        secondRow[10] = symbol;
+                    }
+                }
+                else if (n == 7)
+                {
+                    if (thirdRow[2] == 'X' || thirdRow[2] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        thirdRow[2] = symbol;
+                    }
+                }
+                else if (n == 8)
+                {
+                    if (thirdRow[6] == 'X' || thirdRow[6] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        thirdRow[6] = symbol;
+                    }
+                }
+                else if (n == 9)
+                {
+                    if (thirdRow[10] == 'X' || thirdRow[10] == 'O')
+                    {
+                        a--;
+                        dontClear = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You can't place it there, the box is already occupied. Type a number again: ");
+                    }
+                    else
+                    {
+                        thirdRow[10] = symbol;
+                    }
+                }
+                else
+                {
+                    a--;
+                    dontClear = true;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You can only type the numbers from 1 to 9");
+                }
+                if(dontClear == false)
+                {
+                    Console.Clear();
+                }                
+                foreach (char c in firstRow)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+                foreach (char c in secondRow)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+                foreach (char c in thirdRow)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+                if (firstRow[2] == symbol && firstRow[6] == symbol && firstRow[10] == symbol)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                    Console.WriteLine($"│ {symbol} │ {symbol} │ {symbol} │");
+                    Console.WriteLine("│   │   │   │");
+                    Console.WriteLine("│   │   │   │");
                     break;
                 }
-                else if(firstRow[1] == '0' & firstRow[5] == '0' & firstRow[9] == '0')
+                else if (secondRow[2] == symbol && secondRow[6] == symbol && secondRow[10] == symbol)
                 {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine(" 0 | 0 | 0 ");
-                    Console.WriteLine("   |   |   ");
-                    Console.WriteLine("   |   |   ");
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("│   │   │   │");
+                    Console.WriteLine($"│ {symbol} │ {symbol} │ {symbol} │");
+                    Console.WriteLine("│   │   │   │");
                     break;
                 }
-                if (secondRow[1] == 'X' & secondRow[5] == 'X' & secondRow[9] == 'X')
+                else if (thirdRow[2] == symbol && thirdRow[6] == symbol && thirdRow[10] == symbol)
                 {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("   |   |   ");
-                    Console.WriteLine(" X | X | X ");
-                    Console.WriteLine("   |   |   ");
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("│   │   │   │");
+                    Console.WriteLine("│   │   │   │");
+                    Console.WriteLine($"│ {symbol} │ {symbol} │ {symbol} │");
                     break;
                 }
-                else if (secondRow[1] == '0' & secondRow[5] == '0' & secondRow[9] == '0')
+                else if (firstRow[2] == symbol && secondRow[2] == symbol && thirdRow[2] == symbol)
                 {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("   |   |   ");
-                    Console.WriteLine(" 0 | 0 | 0 ");
-                    Console.WriteLine("   |   |   ");
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                    Console.WriteLine($"│ {symbol} │   │   │");
+                    Console.WriteLine($"│ {symbol} │   │   │");
+                    Console.WriteLine($"│ {symbol} │   │   │");
                     break;
                 }
-                if (thirdRow[1] == 'X' & thirdRow[5] == 'X' & thirdRow[9] == 'X')
+                else if (firstRow[6] == symbol && secondRow[6] == symbol && thirdRow[6] == symbol)
                 {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("   |   |   ");
-                    Console.WriteLine("   |   |   ");
-                    Console.WriteLine(" X | X | X ");
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                    Console.WriteLine($"│   │ {symbol} │   │");
+                    Console.WriteLine($"│   │ {symbol} │   │");
+                    Console.WriteLine($"│   │ {symbol} │   │");
                     break;
                 }
-                else if (thirdRow[1] == '0' & thirdRow[6] == '0' & thirdRow[9] == '0')
+                else if (firstRow[10] == symbol && secondRow[10] == symbol && thirdRow[10] == symbol)
                 {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("   |   |   ");
-                    Console.WriteLine("   |   |   ");
-                    Console.WriteLine(" 0 | 0 | 0 ");
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                    Console.WriteLine($"│   │   │ {symbol} │");
+                    Console.WriteLine($"│   │   │ {symbol} │");
+                    Console.WriteLine($"│   │   │ {symbol} │");
                     break;
                 }
-                if(firstRow[1] == 'X' & secondRow[1] == 'X' & thirdRow[1] == 'X')
+                else if (firstRow[2] == symbol && secondRow[6] == symbol && thirdRow[10] == symbol)
                 {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine(" X |   |    ");
-                    Console.WriteLine(" X |   |    ");
-                    Console.WriteLine(" X |   |    ");
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                    Console.WriteLine($"│ {symbol} │   │   │");
+                    Console.WriteLine($"│   │ {symbol} │   │");
+                    Console.WriteLine($"│   │   │ {symbol} │");
                     break;
                 }
-                else if(firstRow[1] == '0' & secondRow[1] == '0' & thirdRow[1] == '0')
+                else if (firstRow[10] == symbol && secondRow[6] == symbol && thirdRow[2] == symbol)
                 {
-                    Console.WriteLine("----------");
-                    Console.WriteLine(" 0 |   |   ");
-                    Console.WriteLine(" 0 |   |   ");
-                    Console.WriteLine(" 0 |   |   ");
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                    Console.WriteLine($"│   │   │ {symbol} │");
+                    Console.WriteLine($"│   │ {symbol} │   │");
+                    Console.WriteLine($"│ {symbol} │   │   │");
                     break;
                 }
-                if (firstRow[5] == 'X' & secondRow[5] == 'X' & thirdRow[5] == 'X')
-                {
-                    Console.WriteLine("----------");
-                    Console.WriteLine("   | X |  ");
-                    Console.WriteLine("   | X |  ");
-                    Console.WriteLine("   | X |  ");
-                    break;
-                }
-                else if (firstRow[5] == '0' & secondRow[5] == '0' & thirdRow[5] == '0')
-                {
-                    Console.WriteLine("----------");
-                    Console.WriteLine("   | 0 |  ");
-                    Console.WriteLine("   | 0 |  ");
-                    Console.WriteLine("   | 0 |  ");
-                    break;
-                }
-                if (firstRow[9] == 'X' & secondRow[9] == 'X' & thirdRow[9] == 'X')
-                {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("   |   | X ");
-                    Console.WriteLine("   |   | X ");
-                    Console.WriteLine("   |   | X ");
-                    break;
-                }
-                else if (firstRow[9] == '0' & secondRow[9] == '0' & thirdRow[9] == '0')
-                {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("   |   | 0 ");
-                    Console.WriteLine("   |   | 0 ");
-                    Console.WriteLine("   |   | 0 ");
-                    break;
-                }
-                if(firstRow[1] == 'X' & secondRow[5] == 'X' & thirdRow[9] == 'X')
-                {
-                    Console.WriteLine("---------- ");
-                    Console.WriteLine(" X |   |   ");
-                    Console.WriteLine("   | X |   ");
-                    Console.WriteLine("   |   | X ");
-                    break;
-                }
-                else if(firstRow[1] == '0' & secondRow[5] == '0' & thirdRow[9] == '0')
-                {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine(" 0 |   |   ");
-                    Console.WriteLine("   | 0 |   ");
-                    Console.WriteLine("   |   | 0 ");
-                    break;
-                }
-                if (firstRow[9] == 'X' & secondRow[5] == 'X' & thirdRow[1] == 'X')
-                {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("   |   | X ");
-                    Console.WriteLine("   | X |   ");
-                    Console.WriteLine(" X |   |   ");
-                    break;
-                }
-                else if (firstRow[9] == '0' & secondRow[5] == '0' & thirdRow[1] == '0')
-                {
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("   |   | 0 ");
-                    Console.WriteLine("   | 0 |   ");
-                    Console.WriteLine(" 0 |   |   ");
-                    break;
-                }
-
-
-                x = int.Parse(Console.ReadLine());
 
             }
-
-
-
         }
     }
 }
